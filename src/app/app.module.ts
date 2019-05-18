8a8eb7a0883c167ce21e1e11ca731df385745ea2
Estudiante Politecnico Grancolombiano <general@poligran.edu.co>
2019-05-18 14:03:42 -0500

    Componentes peliculas e info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PeliculasComponent } from './Vistas/peliculas/peliculas.component';
+import { InfoComponent } from './Vistas/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculasComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
